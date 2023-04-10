5f15ad76fb3a9b2077d9367464a0280900db7c7d
Your Name <you@example.com>
2023-04-10 17:29:01 -0400

    Very cool ngl. Cool!!!!

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Vocabulate - Powered by OpenAI" key="title"/>
+        <meta property="og:description" content="Find any word you want just by describing it" key="description"/>
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
       <body>
